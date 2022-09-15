2088afe213ecac06fa7060af06fdd82b723dab0c
gui vieira <guivieirasilva02@gmail.com>
2022-09-15 11:35:43 -0300

    parte da landing page web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,23 @@
 import './styles/main.css'
 
+import LogoImg from "./assets/logo-nlw-esports.svg"
+
+import { Cards } from './components/Cards'
+import { Footer } from './components/Footer'
+
 function App() {
   return (
-    <div className='w-8 h-8 bg-violet-500 '>
+    <main className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={LogoImg} alt='Logo da NLW edição eSports' />
 
-    </div>
+      <h1 className='text-6xl text-white font-black mt-20' > 
+        Seu <span className='text-transparent bg-clip-text bg-nlw-gradient'>duo</span> está aqui
+      </h1>
+
+      <Cards />
+      <Footer />
+      
+    </main>
   )
 }
 
