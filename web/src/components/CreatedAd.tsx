6a1cec123aafa22c410ba6411747cc12ff6ecc47
gui vieira <guivieirasilva02@gmail.com>
2022-09-16 20:08:47 -0300

    cards

--- web/src/components/CreatedAd.tsx
+++ web/src/components/CreatedAd.tsx
@@ -1,6 +1,9 @@
 import { MagnifyingGlassPlus } from "phosphor-react"
 
-export function Footer(){
+import * as Dialog from "@radix-ui/react-dialog"
+
+
+export function CreatedAd(){
    return(
       <footer className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8 ' >
         <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
@@ -8,8 +11,9 @@ export function Footer(){
             <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
             <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
           </div>
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3 duration-500 hover:scale-105'>
-            <MagnifyingGlassPlus size={24} /> Publicar Anúncio</button>
+          <Dialog.Trigger className='py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3 duration-500 hover:scale-105'>
+            <MagnifyingGlassPlus size={24} /> Publicar Anúncio
+          </Dialog.Trigger>
         </div>
       </footer>
    )
