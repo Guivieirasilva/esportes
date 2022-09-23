2143663d36a6c36120734573f9254afb31616475
gui vieira <guivieirasilva02@gmail.com>
2022-09-23 16:18:27 -0300

    tela de tablet e mobile

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -6,7 +6,7 @@ interface GameBannerProps {
 
 export function GameBanner(props: GameBannerProps){
    return(
-      <a href='' className='relative rounded-lg overflow-hidden duration-500 hover:scale-105'>
+      <a href='#' onClick={(event) => event.preventDefault()} className='relative rounded-lg overflow-hidden duration-500 hover:scale-105'>
          <img src={props.bannerUrl} alt='banner do game' />
          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
             <strong className='font-bold text-white block'>{props.title}</strong>
