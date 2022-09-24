46ee127e373bbc63d7772cf05e29c4c5ddf9d20f
gui vieira <guivieirasilva02@gmail.com>
2022-09-24 19:35:18 -0300

    notificações no app

---
+++ mobile/src/services/getPushNotificationToken.ts
@@ -0,0 +1,16 @@
+import * as Notifications from 'expo-notifications'   
+
+export async function getPushnotificationToken(){
+   const { granted } = await Notifications.getPermissionsAsync();
+
+   if(!granted){
+      await Notifications.requestPermissionsAsync();
+   }
+
+   if(granted){
+      const pushToken = await Notifications.getExpoPushTokenAsync();
+      console.log("Nova notificação => ", pushToken.data);
+      
+      return pushToken.data
+   }
+}
